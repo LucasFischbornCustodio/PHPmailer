453cc24ca65b2a50b4b6956540ebce2638b58882
LucasFischbornCustodio <93406817+LucasFischbornCustodio@users.noreply.github.com>
2023-05-10 23:53:48 -0300

    Add files via upload

--- php_email.com/send.php
+++ php_email.com/send.php
@@ -1,21 +1,22 @@
 <?php
 
-    require 'mailer/PHPMailerAutoload.php';
-    require 'PHPMailer-master/src/Exception.php';
-    require 'PHPMailer-master/src/PHPMailer.php';
-    require 'PHPMailer-master/src/SMTP.php';
+    
+    require_once 'PHPMailer/src/Exception.php';
+    require_once 'PHPMailer/src/PHPMailer.php';
+    require_once 'PHPMailer/src/SMTP.php';
 //dar require dos arquivos da pasta source?? php mailer smtp exeption
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
     
 
-    if(isset($_POST["send"])){
+    if(isset($_POST["enviar"])){
         $mail = new PHPMailer(true);
 
         $mail-> isSMTP();
         $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAauth = true;
+        $mail->SMTPAuth = true;
         $mail->Username = 'exercmail@gmail.com';
         $mail->Password = 'hdggoohjhwlrvbjc';
         $mail->SMTPSecure = 'ssl';
@@ -23,12 +24,12 @@ use PHPMailer\PHPMailer\Exception;
 
         $mail->setFrom('exercmail@gmail.com');
 
-        $mail->addAdress($_POST["email"]);
+        $mail->addAddress($_POST["email"]);
 
         $mail->isHTML(true);
 
-        $mail->Assunto = $_POST["assunto"];
-        $mail->Text = $_POST["mensagem"];  
+        $mail->Subject = $_POST["assunto"];
+        $mail->Body = $_POST["mensagem"];  
 
         $mail->send();
 
